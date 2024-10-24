fa8c8b84339212dd093a54e4d0ffc436a7d3a58f
nashir <nashirnoor1718@gmail.com>
2024-10-24 13:46:31 +0530

    adjf

--- chat/serializers.py
+++ chat/serializers.py
@@ -2,9 +2,26 @@ from rest_framework import serializers
 from .models import Message, ChatRoom
 
 class MessageSerializer(serializers.ModelSerializer):
+    sender = serializers.SerializerMethodField()
+    receiver = serializers.SerializerMethodField()
+
     class Meta:
         model = Message
-        fields = '__all__'
+        fields = ['id', 'content', 'content_type', 'created_at', 'sender', 'receiver', 'status']
+
+    def get_sender(self, obj):
+        return {
+            'id': str(obj.sender.id),  
+            'username': obj.sender.username,
+            'user_type': obj.sender.user_type
+        }
+
+    def get_receiver(self, obj):
+        return {
+            'id': str(obj.receiver.id),  
+            'username': obj.receiver.username,
+            'user_type': obj.receiver.user_type
+        }
 
 class ChatRoomSerializer(serializers.ModelSerializer):
     class Meta:
--- chat/views.py
+++ chat/views.py
@@ -3,6 +3,7 @@ from rest_framework.permissions import IsAuthenticated
 from .models import ChatRoom, Message
 from .serializers import ChatRoomSerializer, MessageSerializer
 from django.db import models
+from django.db.models import Q 
 
 class ChatRoomListCreateView(generics.ListCreateAPIView):
     serializer_class = ChatRoomSerializer
@@ -29,5 +30,20 @@ class ChatRoomMessagesView(generics.ListAPIView):
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
-        chat_room_id = self.kwargs['pk']
-        return Message.objects.filter(chat_room_id=chat_room_id).order_by('-created_at')
+        other_user_id = self.kwargs['pk']  # This could be either driver_id or customer_id
+        current_user = self.request.user
+
+        chat_room = ChatRoom.objects.filter(
+            (Q(driver=current_user) & Q(customer_id=other_user_id)) |
+            (Q(customer=current_user) & Q(driver_id=other_user_id))
+        ).first()
+
+        if chat_room:
+            Message.objects.filter(
+                chat_room=chat_room,
+                receiver=current_user,
+                status='unread'
+            ).update(status='read')
+            
+            return Message.objects.filter(chat_room=chat_room).order_by('created_at')
+        return Message.objects.none()
